d227e12ac56df271e4a66d28713fa34d0659d4ec
Pálóczi Ádám <palocziad@gmail.com>
2020-03-05 16:13:27 +0100

    image resize, extract s3upload, upload both sizes

--- controllers/spokes-controller.js
+++ controllers/spokes-controller.js
@@ -2,6 +2,8 @@
 
 const mongoose = require('mongoose');
 const Wheel = require('../models/wheel');
+const Jimp = require('jimp');
+const s3Upload = require('../tools/s3-upload')
 
 const AWS = require('aws-sdk');
 AWS.config.update({
@@ -15,40 +17,17 @@ const calculateDistance = require('../tools/distance');
 
 const postSpoke = async (req, res, next) => {
   //check user
+  //if image -> metadata, if exif -> distance,  confirmation none/photo/manual
+  //return: resized url, distance, spoke
   console.log('POST spoke');
 
   const image = req.file;
+  let resizedImage;
   let metadata;
-
-  try {
-    metadata = await extractExif(image.buffer);
-  } catch (error) {
-    console.log(error);
-    const errorResponse = new Error('Error extracting exif');
-    errorResponse.errorCode = 500; 
-    return next(errorResponse);
-}
-
-  const spot = JSON.parse(req.body.spot);
-  const spotCoordinates = {
-    lat: spot.location.lat,
-    lng: spot.location.lng
-  };
-  const exifDec = {
-    lat: metadata.gps.GPSLatitude[0] + metadata.gps.GPSLatitude[1]/60 + metadata.gps.GPSLatitude[2]/3600,
-    lng: metadata.gps.GPSLongitude[0] + metadata.gps.GPSLongitude[1]/60 + metadata.gps.GPSLongitude[2]/3600
-  };
-  const distance = calculateDistance(exifDec, spotCoordinates);
-  let imageDate = null
-  if (metadata.exif.DateTimeOriginal) {
-    let date = metadata.exif.DateTimeOriginal.split(" ");
-    date[0] = date[0].replace(/:/g,"-");
-    imageDate = Date.parse(date.join("T"));
-  }
-
   let wheel;
   let spoke = {}
-  let url;
+  let url, resizedUrl;
+  const spot = JSON.parse(req.body.spot);
 
   try {
     wheel = await Wheel.findById(spot.wheel).populate({path: 'trip', select: 'confirmation'});
@@ -59,18 +38,68 @@ const postSpoke = async (req, res, next) => {
     errorResponse.errorCode = 500; 
     return next(errorResponse);
   }
-  
+
+  //if image
+
+  // Jimp.read(image.buffer)
+  //   .then(async (image) => {
+  //     image.resize(Jimp.AUTO, 1200);
+  //     resizedImage = await image.getBufferAsync(Jimp.AUTO);
+  //     console.log(resizedImage);
+  //   })
+  //   .catch((error) => {
+  //     console.log(error);
+  //     const errorResponse = new Error('Error resizing image');
+  //     errorResponse.errorCode = 500; 
+  //     return next(errorResponse);
+  //   });
+
+    let jImage
+    try {
+      jImage = await Jimp.read(image.buffer);
+      jImage.resize(Jimp.AUTO, 1080);
+      jImage.quality(90);
+      resizedImage = await jImage.getBufferAsync(Jimp.AUTO);
+    } catch (error) {
+      
+    }
+
   try {
-    const s3 = new AWS.S3({signatureVersion: 'v4'});
-    const s3Params = { 
-      Bucket: process.env.SPOKET_S3_BUCKET,
-      Body: image.buffer,
-      Key: `${Date.now()}_original_${image.originalname}`,
-      ContentType: image.mimetype,
-      Expires: 900} //default
-    await s3.upload(s3Params).promise();
-    spoke.image.key = s3Params.Key
-    url = s3.getSignedUrl('getObject', {Bucket: s3Params.Bucket, Key: s3Params.Key});
+    metadata = await extractExif(image.buffer);
+  } catch (error) {
+    console.log(error);
+    const errorResponse = new Error('Error extracting exif');
+    errorResponse.errorCode = 500; 
+    return next(errorResponse);
+  }
+
+  const spotCoordinates = {
+    lat: spot.location.lat,
+    lng: spot.location.lng
+  };
+
+  const exifDec = {
+    lat: metadata.gps.GPSLatitude[0] + metadata.gps.GPSLatitude[1]/60 + metadata.gps.GPSLatitude[2]/3600,
+    lng: metadata.gps.GPSLongitude[0] + metadata.gps.GPSLongitude[1]/60 + metadata.gps.GPSLongitude[2]/3600
+  };
+
+  const distance = calculateDistance(exifDec, spotCoordinates);
+  let imageDate = null
+  if (metadata.exif.DateTimeOriginal) {
+    let date = metadata.exif.DateTimeOriginal.split(" ");
+    date[0] = date[0].replace(/:/g,"-");
+    imageDate = Date.parse(date.join("T"));
+  }
+
+  try {
+    let uploadPromises = [];
+    spoke.image.key = `photos/trip_${wheel.trip._id}/wheel_${wheel._id}/spoke_${spoke._id}/${Date.now()}_original_${image.originalname}`
+    uploadPromises.push(s3Upload(spoke.image.key, image.buffer, image.mimetype))
+    spoke.image.resizedKey = `photos/trip_${wheel.trip._id}/wheel_${wheel._id}/spoke_${spoke._id}/${Date.now()}_resized_${image.originalname}`
+    uploadPromises.push(s3Upload(spoke.image.resizedKey, resizedImage, image.mimetype));
+    const uploadResults = await Promise.all(uploadPromises);
+    resizedUrl = uploadResults[1];
+    url = uploadResults[0];
   } catch (error) {
     console.log(error);
     const errorResponse = new Error('Error using bucket');
@@ -78,6 +107,8 @@ const postSpoke = async (req, res, next) => {
     return next(errorResponse);
   }
 
+  //end if image 
+  
   try {
     const spokeVerified = (wheel.trip.confirmation === 'none' || (wheel.trip.confirmation === 'photo' && distance < 150)) ? new Date() : null;
     const spokeLocation = spokeVerified ? spot.location : exifDec;
@@ -94,7 +125,6 @@ const postSpoke = async (req, res, next) => {
     } else {
       wheel.spokes.push(spoke);
     }
-    //wheel.spokes[wheel.spokes.findIndex((spokeToFind) => toString(spokeToFind._id) === toString(spoke._id))] = spoke;
     await wheel.save();
   } catch (error) {
     console.log(error);
@@ -104,9 +134,9 @@ const postSpoke = async (req, res, next) => {
   }
   
   if (metadata.gps.GPSLatitude) {
-    res.status(200).json({gps: metadata.gps, distance, spoke, url});
+    res.status(200).json({gps: metadata.gps, distance, spoke, resizedUrl});
   } else {
-    res.status(200).json({gps: "N/A", distance: "N/A", spoke, url});
+    res.status(200).json({gps: "N/A", distance: "N/A", spoke, resizedUrl});
   }
 };
 
---
+++ tools/s3-upload.js
@@ -0,0 +1,28 @@
+const AWS = require('aws-sdk');
+AWS.config.update({
+  accessKeyId: process.env.SPOKET_AWSAccessKeyId,
+  secretAccessKey: process.env.SPOKET_AWSSecretKey,
+  region: 'eu-central-1'
+});
+
+const s3Upload = async (key, buffer, mimetype) => {
+  return new Promise(async (resolve, reject) => {
+    const s3 = new AWS.S3({signatureVersion: 'v4'});
+    const s3Params = { 
+      Bucket: process.env.SPOKET_S3_BUCKET,
+      Body: buffer,
+      Key: key,
+      ContentType: mimetype,
+      Expires: 900} //default
+      try {
+        await s3.upload(s3Params).promise()
+        let url = s3.getSignedUrl('getObject', {Bucket: s3Params.Bucket, Key: s3Params.Key});
+        resolve(url)
+      } catch (error) {
+        reject(error)
+      }
+  })
+
+}
+
+module.exports = s3Upload;
